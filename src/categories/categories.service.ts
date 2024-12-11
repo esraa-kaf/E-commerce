7f2586a7d078f16d3c6bd907747494dde2ee6b0c
esraa-kaf <esraakaf3@gmail.com>
2024-12-11 11:17:34 +0200

    create refactor

--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -1,50 +1,16 @@
 import { Categories } from './categories.interface';
-import asyncHandler from 'express-async-handler';
-import{Request ,Response ,NextFunction }from 'express'
 import categoriesSchema from './categories.schema';
+import refactorService from '../../refactor.service';
 class CategoriesService{
 
-// create categories
-   createOne= asyncHandler( async (req:Request , res:Response)=>{
-    const Category:Categories = await categoriesSchema.create(req.body);
-    res.status(201).json({data:Category});
-  
-})
-// getall categories
-    async getAll(req:Request , res:Response){
-        const categories:Categories[] =await categoriesSchema.find();
-        res.status(200).json({data:categories});
+    createOne=refactorService.createOne<Categories>(categoriesSchema);
+    getAll= refactorService.getAll<Categories>(categoriesSchema);
+    getOne= refactorService.getOne<Categories>(categoriesSchema);
+    updateOne=refactorService.updateOne<Categories>(categoriesSchema);
+    deleteOne=refactorService.deleteOne<Categories>(categoriesSchema);
+
 
 }
-
-// get one category
- async getOne(req:Request , res:Response){
-    const category:Categories | null = await categoriesSchema.findById(req.params.id);
-    if(category){
-        res.status(200).json({data:category});
-    }else{
-        res.status(404).json(`${req.__('not_found')}`);
-    }
- }
-
-
-
-// update category
- updateOne=asyncHandler( async (req:Request , res:Response)=>{
-    const updatedCategory:Categories|null = await categoriesSchema.findByIdAndUpdate(req.params.id, req.body,{new:true});
-    res.status(200).json({data:updatedCategory});
-
-})
-// delete category
- deleteOne=asyncHandler( async (req:Request , res:Response)=>{
-    const deleteCategory:Categories|null =  await categoriesSchema.findByIdAndDelete(req.params.id);
-    res.status(204).json();
-
-})
- }
-
-
-
 // اسم الكلااس لازم يبقى حرف كابيتال
 const categoriesService =new CategoriesService()
 export  default categoriesService;
--- src/subcategories/subcategories.service.ts
+++ src/subcategories/subcategories.service.ts
@@ -1,42 +1,27 @@
-import asyncHandler from 'express-async-handler';
 import { Subcategories } from './subcategories.interface';
 import {Request , Response ,NextFunction} from 'express'
 import subcategoriesSchema from './subcategories.schema';
+import refactorService from '../../refactor.service';
 
 class SubcategoriesService{
-// create a new Subcategories
- createOne=asyncHandler( async(req:Request , res:Response)=>{
+   setCategoryId(req: Request, res: Response,next:NextFunction){
     if(req.params.categoryId&&!req.body.category) req.body.category = req.params.categoryId;
-    const subcategory:Subcategories=await subcategoriesSchema.create(req.body)
-    res.status(201).json({data:subcategory})
-})
-// getall subcategories
- getAll =asyncHandler(async (req:Request , res:Response)=>{
-    const filterData:any={};
-    if(req.params.categoryId) filterData.categoryId = req.params.categoryId;
-    const Subcategories:Subcategories[] =await subcategoriesSchema.find(filterData);
-    res.status(200).json({data:Subcategories});
+     next();
+   }
+    
+   filterSubcategories(req: Request, res: Response , next:NextFunction){
+    const filterData:any ={};
+    if(req.params.categoryId) filterData.category = req.params.categoryId;
+    next();
+   }
 
-})
-// get one subcategory
-getOne=asyncHandler(async (req:Request , res:Response)=>{
-    const Subcategories:Subcategories[] | null =await subcategoriesSchema.findById(req.params.id);
-    res.status(200).json({data:Subcategories});
-})
+   createOne=refactorService.createOne<Subcategories>(subcategoriesSchema);
+   getAll= refactorService.getAll<Subcategories>(subcategoriesSchema);
+   getOne= refactorService.getOne<Subcategories>(subcategoriesSchema);
+   updateOne=refactorService.updateOne<Subcategories>(subcategoriesSchema);
+   deleteOne=refactorService.deleteOne<Subcategories>(subcategoriesSchema);
 
 
-// update subcategory
- updateOne=asyncHandler(async (req:Request , res:Response)=>{
-    const updatedSubcategories:Subcategories | null = await subcategoriesSchema.findByIdAndUpdate(req.params.id, req.body, {new: true});
-    res.status(200).json({data:updatedSubcategories});
-})
-
-// delete subcategory
- deleteOne=asyncHandler(async (req:Request , res:Response)=>{
-    const deleteSubcategories:Subcategories | null = await subcategoriesSchema.findByIdAndDelete(req.params.id);
-    res.status(204).json();
-
-})
 
 
 
